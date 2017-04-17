ba55f352e5f27d557be7dbd522dc1b6b4e471fe7
TheGamingViking <Frederik@Taaning-Grundholm.dk>
2017-04-17 12:29:45 +0200

    New Schedule type. Dictionary list. Functional but needs code for auto backup.

--- FileBackupSystem FFM/FileBackupSystem FFM/Schedule.cs
+++ FileBackupSystem FFM/FileBackupSystem FFM/Schedule.cs
@@ -9,68 +9,58 @@ namespace FileBackupSystem_FFM
 {
     public class Schedule
     {
-        DateTime midnight = new DateTime(2017, 4, 14, 23, 59, 59, 999);
-        DateTime deadline = new DateTime(2017, 4, 14, 0, 1, 0, 0);
-        DateTime dt = new DateTime();
-        int i = 1;
-        int j = 1;
-        List<string> userdays = new List<string> { "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Saturday", "Sunday" };
-        List<string> usertimes = new List<string> { "11:00", "15:00", "02:00", "12:00", "09:00", "19:00", "20:00" };
-
         public void DateAndTimeCheck()
         {
-            dt = DateTime.Now;
-            foreach (var day in userdays)
+            Dictionary <string, List<string>> schedule = new Dictionary<string, List<string>>() ;
+            #region DictionaryFIll
+            List<string> Monday = new List<string>();
+            Monday.Add("09:00");
+            Monday.Add("12:00");
+            Monday.Add("15:00");
+            schedule.Add("Monday", Monday);
+            #endregion
+            DateTime conversion;
+            string temp;
+            int x = 60;
+            for (int i = 0; i < schedule.Count; i++)
             {
-                if (day == dt.DayOfWeek.ToString())
+                conversion = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+                if (schedule.Keys.ElementAt(i).ToLower() == conversion.DayOfWeek.ToString().ToLower())
                 {
-                    MessageBox.Show("Correct day");
-
-                    foreach (var time in usertimes)
+                    foreach (string time in schedule.Values.ElementAt(i))
                     {
-                        string time1 = time.Remove(2);
-                        if ((time1.Trim('0') == dt.Hour.ToString())&&(i==j))
+                        temp = time;
+                        temp = temp.Remove(2);
+                        TimeSpan ts = new TimeSpan(Convert.ToInt32(temp.TrimStart('0')), DateTime.Now.Minute, DateTime.Now.Second);
+                        conversion = conversion.Date + ts;
+                        MessageBox.Show(Convert.ToString(conversion.AddMinutes(x)));
+                        if (conversion.AddMinutes(x) > DateTime.Now && conversion < DateTime.Now)
                         {
-                            MessageBox.Show("The time is right");
-                            //kør autobackup
-                            break;
+                            MessageBox.Show("Fuck Yeah");
+                            //Run auto backup
                         }
-                        else if (i==j)
-                        {
-                            TimeSpan deadlinets = new TimeSpan(Convert.ToInt32(time1.Trim('0')), 0, 0);
-                            deadline = deadline.Date + deadlinets;
-                            TimeSpan duration = deadline.Subtract(dt);
-                            if (Convert.ToString(duration).Contains('-'))
-                            {
-                                MessageBox.Show(Convert.ToString(duration));
-                                MidNightEvent();
-                                break;
-                            }
-                            else
-                            {
-                                //lav event til køre DateAndTimeCheck() igen når tiden i duration er 0
-                                break;
-                            }
-                            
-                            
-                        }
-                        j++;
                     }
-                    break;
                 }
-                else if (i==userdays.Count)
-                {
-                    MidNightEvent();
-                }
-                i++;
             }
             
-        }
-        public void MidNightEvent()
-        {
-            TimeSpan duration = midnight.Subtract(dt);
-            MessageBox.Show(Convert.ToString(duration));
-            //lav event til køre DateAndTimeCheck() basert på duration så vi har passeret midnat.
+            
+            /*
+            for (int i = 0; i < schedule.Values.Count; i++)
+            {
+                //Check time.Now vs. schedule.Values[i]
+                temp = schedule.Values.ElementAt(i).Remove(2);
+                conversion = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, Convert.ToInt32(temp.TrimStart('0')), DateTime.Now.Minute, DateTime.Now.Second);
+                if (conversion > DateTime.Now && conversion < DateTime.Now.AddMinutes(x))
+                {
+                    if (schedule.Keys.ElementAt(i).ToLower() == conversion.DayOfWeek.ToString().ToLower())
+                    {
+
+                    }
+                }
+            }
+            */
+
+
         }
     }
 }
